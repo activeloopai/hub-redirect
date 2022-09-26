82f2f288be79a138cac5f6c13a3aec4d21e66d1f
Fariz Rahman <farizrahman4u@gmail.com>
2022-09-26 04:12:21 +0400

    fix typi

--- hub/__init__.py
+++ hub/__init__.py
@@ -2,8 +2,7 @@ import deeplake
 import warnings
 
 warnings.warn(
-    "Hub is deprecated. Use Deep Lake instead:\n!pip install deelplake\nimport deeplake",
+    "Hub is deprecated. Use Deep Lake instead:\n!pip install deeplake\nimport deeplake",
     DeprecationWarning,
-    stacklevel=2,
 )
 globals().update(deeplake.__dict__)  # Forgive me
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 config = {
     "name": "hub-redirect",
-    "version": "3.0.1",
+    "version": "3.0.2",
     "description": "Activeloop Deep Lake",
     "long_description": "Use deeplake instead: pip install deeplake",
     "long_description_content_type": "text/x-rst",
