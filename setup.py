0a99966505aa9250f6f5e1c65b3189fc419f6e26
Fariz Rahman <farizrahman4u@gmail.com>
2022-09-27 12:00:59 +0400

    3.0.3

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 
 config = {
-    "name": "hub",
-    "version": "2.8.6",
-    "description": "Activeloop Hub",
-    "long_description": "...",
+    "name": "hub-redirect",
+    "version": "3.0.3",
+    "description": "Activeloop Deep Lake",
+    "long_description": "Use deeplake instead: pip install deeplake",
     "long_description_content_type": "text/x-rst",
     "author": "activeloop.ai",
     "author_email": "support@activeloop.ai",
     "install_requires": ["deeplake"],
     "zip_safe": False,
-    "url": "https://www.github.com/activeloopai/hub",
+    "url": "https://www.github.com/activeloopai/deeplake",
 }
 
 setup(**config)
